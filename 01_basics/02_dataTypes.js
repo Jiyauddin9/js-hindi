2c303f519db9ecb9d176ca00c4d312846e318cf6
Md. Jiyauddin <jiyauddinmd485@gmail.com>
2023-02-20 20:15:57 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    + 
+    3) // code readability should be high. This is not a good practice.
+
+    console.log("Jiyauddin")
+
+    // data types
+let name = "Jiyauddin" //string
+let age = 18 //number
+let isLoggedIn = true  //boolean
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol 
+
+ console.log(typeof undefined) // undefined 
+ console.log(typeof null) // object
