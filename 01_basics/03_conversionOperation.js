9c0626207b98130a20bebaf5bec5aed3395aa616
Md. Jiyauddin <jiyauddinmd485@gmail.com>
2023-02-21 20:31:37 +0530

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "3abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Conversion in number
+
+// let score = "33" => 33 output
+// let score = "33abc" => NaN output (Not a Number)
+// let score = true => 1; false => 0 output
+// let score = null => 0 output
+// let score = undefined => NaN output
+// let score = "Jani" +> NaN output
+
+// Conversion in boolean
+
+// let isLoggedIn = "Jani"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//let isLoggedIn = 1 => true; 0 => false
+//let isLoggedIn = "" => false
+//let isLoggedIn = "jani" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
