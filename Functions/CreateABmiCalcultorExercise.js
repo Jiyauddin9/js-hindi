75b634c0436a6d9be72f2cd7d927f08c6a7ad962
Md. Jiyauddin <jiyauddinmd485@gmail.com>
2023-02-25 19:38:06 +0530

    BMI Calculator

---
+++ Functions/CreateABmiCalcultorExercise.js
@@ -0,0 +1,23 @@
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+//formula BMI = weight / height * height;
+
+function bmiCalculator(weight, height){
+    // let bmi = weight / (height * height); or
+    let bmi = weight / Math.pow(height,2);
+    return Math.round(bmi);
+}
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+Link:-
+https://www.calculatorsoup.com/calculators/health/bmi-calculator.php
+*/
+
+
+let bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
